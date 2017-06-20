1abb5d68c0d06225e7cb9d9410a33fbc9591ee09
Patrick Levesque <patricklevesque@patricks-mbp.local>
2017-06-19 20:42:47 -0400

    Initial Commit

---
+++ Xcode8Github/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8Github
+//
+//  Created by Patrick Levesque on 2017-06-19.
+//  Copyright © 2017 Patrick Levesque. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8Github/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8Github
+//
+//  Created by Patrick Levesque on 2017-06-19.
+//  Copyright © 2017 Patrick Levesque. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
